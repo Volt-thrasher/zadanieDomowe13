612d24e5cf3e126729aca4bd91bd3f573b97a7c2
Volt <sonianadashkevich@gmail.com>
2019-11-16 20:05:18 +0100

    zadanie1

---
+++ src/z1/Country.java
@@ -0,0 +1,44 @@
+package z1;
+
+public class Country {
+    String index;
+    String name;
+    String population;
+
+    public Country(String index, String name, String population) {
+        this.index = index;
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", (" +
+                index + ") ma " +
+                population + " ludnosci";
+    }
+}
---
+++ src/z1/CountryTest.java
@@ -0,0 +1,45 @@
+package z1;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountryTest {
+    public static void main(String[] args) {
+        try {
+            Map<String, Country> countryMap = createMap();
+            printCountryInfo(countryMap);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void printCountryInfo(Map<String, Country> map) throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj kod kraju, o którym chcesz zobaczyć informacje:");
+        String code = input.nextLine();
+        if (map.get(code) != null) {
+            System.out.println(map.get(code));
+        } else if (map.get(code.toUpperCase()) != null) {
+            System.out.println(map.get(code.toUpperCase()));
+        } else {
+            System.out.println("Nie znaleziono kraju o podanym kodzie");
+        }
+        input.close();
+    }
+
+    private static Map<String, Country> createMap() throws IOException {
+        BufferedReader bfr = new BufferedReader(new FileReader(new File("countries.csv")));
+        String line;
+        Map<String, Country> countryMap = new HashMap<>();
+        while ((line = bfr.readLine()) != null) {
+            String[] countries = line.split(";");
+            countryMap.put(countries[0], new Country(countries[0], countries[1], countries[2]));
+        }
+        bfr.close();
+        return countryMap;
+    }
+}
